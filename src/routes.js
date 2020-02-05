94adaedfd9018f1d3c036789de7c98784bf2a02f
MatheusBrowz <matheuslimaworks@gmail.com>
2020-02-05 07:27:34 -0300

    Adição do controller de usuários e rota Store dos usuários

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,19 @@
-const { Router } = require("express");
+import { Router } from "express";
+import UserController from "./app/controllers/UserController";
 
 const routes = new Router();
 
-routes.get("/", (req, res) => res.json({ hello: "world!" }));
+routes.post("/users", UserController.store);
+
+/*
+routes.get("/", async (req, res) => {
+  const user = await User.create({
+    name: "Diego Fernandes",
+    email: "matheuslimaworks@gmail.com",
+    password_hash: "1234",
+  });
+
+  return res.json(user);
+}); */
 
 export default routes;
